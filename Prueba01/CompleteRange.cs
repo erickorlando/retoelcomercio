00cc6c2218d536a2f832ceeaa2129960e01f5e3f
Erick Orlando <erickorlando@outlook.com>
2017-01-22 15:39:36 -0500

    Problema 02 Resuelto

--- Prueba01/CompleteRange.cs
+++ Prueba01/CompleteRange.cs
@@ -1,7 +1,26 @@
-﻿namespace Prueba01
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Prueba01
 {
     public class CompleteRange
     {
-        
+        public List<int> Build(List<int> coleccion)
+        {
+            var result = new List<int>();
+
+            // Primero ordenamos.
+            var listaOrdenada = coleccion.OrderBy(i => i).ToList();
+            result.AddRange(listaOrdenada);
+
+            for (int index = 0; index < listaOrdenada.Last(); index++)
+            {
+                var next = index + 1;
+                if (!result.Contains(next))
+                    result.Add(next);
+            }
+
+            return result.OrderBy(i => i).ToList();
+        }
     }
 }
--- Prueba01/Program.cs
+++ Prueba01/Program.cs
@@ -1,33 +1,55 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Prueba01
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            // Problema 01
-            /*
-             * entrada:​ "123 abcd*3"salida:​ "123 bcde​*3"
-               entrada:​"**Casa 52"salida:​"**Dbtb​ 52" 
-            */
 
-            var problema01 = new ChangeString();
-            var result = problema01.Build("123 abcd*3");
-            var expected = "123 bcde*3";
+            Titulo("Problema 01");
+            Problema01("123 abcd*3");
+            Problema01("**Casa 52");
 
-            Console.WriteLine("Valor de Result {0}\nValor de Expected {1}", result, expected);
-            if (result == expected)
-                Console.WriteLine("Los valores son iguales");
-
-            result = problema01.Build("**Casa 52");
-            expected = "**Dbtb 52";
-
-            Console.WriteLine("Valor de Result {0}\nValor de Expected {1}", result, expected);
-            if (result == expected)
-                Console.WriteLine("Los valores son iguales");
+            Titulo("Problema 02");
+            Problema02(new List<int> { 2, 1, 4, 5 });
+            Problema02(new List<int> { 4, 2, 9 });
+            Problema02(new List<int> { 58, 60, 55 });
 
             Console.Read();
+        }
+
+        private static void Separador()
+        {
+            Console.WriteLine("=============================");
+        }
+
+        private static void Titulo(string mensaje)
+        {
+            Console.WriteLine("\n");
+            Separador();
+            Console.WriteLine(mensaje);
+            Separador();
+        }
+
+        static void Problema01(string entrada)
+        {
+            var problema01 = new ChangeString();
+            var result = problema01.Build(entrada);
+
+            Console.WriteLine("Valor de Entrada => {0}\nValor de Salida => {1}", entrada, result);
+        }
+
+        static void Problema02(List<int> coleccion)
+        {
+            var problema02 = new CompleteRange();
+            var result = problema02.Build(coleccion);
+
+            foreach (var item in result)
+                Console.Write("{0} ", item);
+
+            Console.WriteLine();
         }
     }
 }
